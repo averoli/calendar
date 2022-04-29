7452e978f1a1c92aeee9a080307312b0c6f86946
Averoli <afanasenkavolha@gmail.com>
2022-04-29 18:05:56 +0200

    for pull

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -4,3 +4,12 @@ document.getElementById("btn__create-event").addEventListener('click', function 
     openModal.classList.remove("hide__element");
 })
 
+const openModalDay = document.querySelectorAll(".table__day");
+
+for(let i = 0; i < openModalDay.length; i++){
+    openModalDay[i].onmouseenter = function(){
+        openModalDay[i].textContent = '<i class="fa-solid fa-circle-plus aria-hidden="true""></i>'
+    }
+}
+
+
