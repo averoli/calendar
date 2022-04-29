1878b4bbc7dcca14b4457f8a7e7d5cf576ae1d11
Averoli <afanasenkavolha@gmail.com>
2022-04-29 15:58:01 +0200

    Changing event to modal

---
+++ src/js/calendar.js
@@ -0,0 +1,6 @@
+const openModal = document.getElementById("modal");
+
+document.getElementById("btn__create-event").addEventListener('click', function (){
+    openModal.classList.remove("hide__element");
+})
+
